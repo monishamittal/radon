0419045c40dea346f68827d1d2bd0e575de28faf
monishamittal <monishamittal0673@gmail.com>
2022-06-02 10:34:03 +0530

    admission/nodejs.modules

--- src/index.js
+++ src/index.js
@@ -11,6 +11,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+    console.log('Express app running on port ' + (process.env.PORT  || 3000))
 });
-
---
+++ src/logger/logger.js
@@ -0,0 +1,14 @@
+const endpoint = "https://www.functionup.org/"
+const batch = "radon"
+
+const log = function() {
+    console.log('I am inside the log function')
+}
+const a=function(){
+    console.log('Welcome to my application. I am Monisha Mittal and a part of FunctionUp Radon cohort.')
+}
+
+module.exports.endpoint=endpoint
+module.exports.batch=batch
+module.exports.log = log
+module.exports.a = a
