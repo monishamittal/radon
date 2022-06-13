313c6934b6d7d5fefb77b7c2611a7feb9c3d358c
monishamittal <monishamittal0673@gamil.com>
2022-06-13 14:03:43 +0530

    assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,11 +4,11 @@ const UserModel= require("../models/userModel")
 
 
 const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    // let tokenDataInHeaders= req.headers.token
+    // console.log(tokenDataInHeaders)
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
+    // console.log( "HEADER DATA ABOVE")
+    // console.log( "hey man, congrats you have reached the Handler")
     res.send({ msg: "This is coming from controller (handler)"})
     }
 
@@ -30,17 +30,17 @@ const basicCode= async function(req, res) {
 
 
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
--- src/index.js
+++ src/index.js
@@ -3,23 +3,29 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment =require('moment');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
+app.use (function (req, res, next) {
+        const today = moment();
+        console.log(today.format("YYYY-MM-DD hh-mm-ss") + ", " + req.ip + ", " + req.path);
+        next()
   }
   );
+
+// mongoose.connect("mongodb+srv://functionup-radon:radon123@cluster0.q0p7q73.mongodb.net/monisha04-DB?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
+
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
 
 app.use('/', route);
 
